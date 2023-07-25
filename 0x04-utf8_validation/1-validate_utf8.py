0012ae42fd99492fb7cff0d7fe3b7d37a76d157f
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2023-07-25 12:53:48 +0300

    func determining if a given set of data represents a valid utf-8 encoding

---
+++ 0x04-utf8_validation/1-validate_utf8.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env pytthon3
+"""
+Determines if a given data set represents a valid UTF-8 encoding
+"""
+
+
+def validUTF8(data):
+    state_of_bytes = 0
+    for num in data:
+        bit = 0b10000000
+        if not state_of_bytes:
+            while (bit & num):
+                state_of_bytes += 1
+                bit >>= 1
+            if state_of_bytes > 4:
+                return False
+            elif state_of_bytes:
+                state_of_bytes -= 1
+                if state_of_bytes == 0:
+                    return False
+        elif state_of_bytes > 0:
+            if num >> 6 != 2:
+                return False
+            state_of_bytes -= 1
+    return not state_of_bytes
